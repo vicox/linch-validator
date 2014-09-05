c03477b61b59143bbc6afeec7acdccf3d142d3aa
Georg Schmidl <georg.schmidl@vicox.net>
2014-09-05 23:34:13 +0200

    add equals other validator

---
+++ src/main/java/com/linchproject/validator/validators/EqualsOtherPropertyValidator.java
@@ -0,0 +1,31 @@
+package com.linchproject.validator.validators;
+
+import com.linchproject.validator.Property;
+import com.linchproject.validator.PropertyValidator;
+
+import java.util.Arrays;
+
+/**
+ * @author Georg Schmidl
+ */
+public class EqualsOtherPropertyValidator implements PropertyValidator {
+
+    private String propertyKey;
+
+    public EqualsOtherPropertyValidator(String propertyKey) {
+        this.propertyKey = propertyKey;
+    }
+
+    @Override
+    public String getKey() {
+        return "equals";
+    }
+
+    @Override
+    public boolean isValid(Property property) {
+        Property otherProperty = property.getData().get(propertyKey);
+
+        String[] otherValues = otherProperty != null ? otherProperty.getStringValues() : null;
+        return Arrays.equals(property.getStringValues(), otherValues);
+    }
+}
---
+++ src/test/java/com/linchproject/validator/validators/EqualsOtherPropertyValidatorTest.java
@@ -0,0 +1,21 @@
+package com.linchproject.validator.validators;
+
+import com.linchproject.validator.Data;
+import com.linchproject.validator.Property;
+import junit.framework.TestCase;
+
+public class EqualsOtherPropertyValidatorTest extends TestCase {
+
+    public void testIsValid() throws Exception {
+        Data data;
+        Property property;
+
+        data = new Data().addProperty("c", "d");
+        property = new Property(data, "a", "b");
+        assertFalse(new EqualsOtherPropertyValidator("c").isValid(property));
+
+        data = new Data().addProperty("c", "b");
+        property = new Property(data, "a", "b");
+        assertTrue(new EqualsOtherPropertyValidator("c").isValid(property));
+    }
+}
