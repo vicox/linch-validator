1de1cf16d6fa1644fa0dd4848644371e4bb1d0a1
Georg Schmidl <georg.schmidl@vicox.net>
2014-09-06 18:42:47 +0200

    add static validators

---
+++ src/main/java/com/linchproject/validator/Validators.java
@@ -0,0 +1,11 @@
+package com.linchproject.validator;
+
+import com.linchproject.validator.validators.EmailValidator;
+
+/**
+ * @author Georg Schmidl
+ */
+public class Validators {
+
+    public static Validator EMAIL = new EmailValidator();
+}
